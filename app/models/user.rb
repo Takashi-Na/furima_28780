24719d9d49052d9cfaa9fb85143735bc9e98a5a3
Takashi-Na <Takashi-Na@users.noreply.github.com>
2020-08-24 14:42:02 +0900

    devise導入　カラムの設定

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :nickname, :family_name, :first_name, :kana_family_name, :kana_first_name, :birthday, presence: true
+end
