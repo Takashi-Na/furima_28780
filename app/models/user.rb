00aaf4c3546ea512e48aeec88fa2b921eff9edb0
Takashi-Na <Takashi-Na@users.noreply.github.com>
2020-08-25 16:46:27 +0900

    ユーザー登録のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :family_name, :first_name, :kana_family_name, :kana_first_name, :birthday, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  validates_format_of :family_name, :first_name, with: NAME_REGEX, message: 'には全角(漢字・ひらがな・カタカナ)で入力してください'
+
+  KANA_NAME_REGEX = /\A[ァ-ン]+\z/
+  validates_format_of :kana_family_name, :kana_first_name, with: KANA_NAME_REGEX, message: 'には全角カタカナで入力してください'
+
 end
