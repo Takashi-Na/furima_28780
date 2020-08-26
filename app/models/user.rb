3956722fe063b687e90120b4397c405bb9567a52
Takashi-Na <Takashi-Na@users.noreply.github.com>
2020-08-26 19:33:01 +0900

    userモデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
-  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
   validates_format_of :family_name, :first_name, with: NAME_REGEX, message: 'には全角(漢字・ひらがな・カタカナ)で入力してください'
 
-  KANA_NAME_REGEX = /\A[ァ-ン]+\z/
+  KANA_NAME_REGEX = /\A[ァ-ン]+\z/.freeze
   validates_format_of :kana_family_name, :kana_first_name, with: KANA_NAME_REGEX, message: 'には全角カタカナで入力してください'
 
 end
