c872513c6a81cbbd649d8d9197a166d34d660694
Takashi-Na <Takashi-Na@users.noreply.github.com>
2020-08-26 19:48:31 +0900

    微修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,11 @@ class ApplicationController < ActionController::Base
 
   protected
 
-<<<<<<< Updated upstream
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :kana_family_name, :kana_first_name, :birthday])
-=======
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
->>>>>>> Stashed changes
   end
 end
