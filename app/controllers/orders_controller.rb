476f2e676d6f3dfe7fa0f82262759d07af8d88c8
Takashi-Na <Takashi-Na@users.noreply.github.com>
2020-09-08 10:31:22 +0900

    購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,33 @@
 class OrdersController < ApplicationController
   def index
-    @order = Item.find(params[:item_id])
+    @item = Item.find(params[:item_id])
+    @order = OrderAddress.new
+  end
+
+  def create
+    @order = OrderAddress.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order.valid?
+      pay_item
+      @order.save
+      redirect_to root_path
+    else
+      render "index"
+    end
+  end
+
+  private
+
+  def pay_item
+    Payjp.api_key = "sk_test_785e9814c7a62311027b0c18"
+    Payjp::Charge.create(
+      amount: @item.price,           # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
+  end
+
+  def order_params
+    params.permit(:post_code, :prefecture, :city, :address, :building_name, :tel_num).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
---
+++ app/forms/order_address.rb
@@ -0,0 +1,18 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :token, :post_code, :prefecture, :city, :address, :building_name, :tel_num, :order
+
+  # カード情報と住所のバリデーション
+  with_options presence: true do
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture, numericality: { other_than: 0 , message: "can't be blank" }
+    validates :user_id, :item_id, :token, :city, :address, :tel_num
+  end
+
+  def save
+    # カード情報の保存
+    order = Order.create( user_id: user_id, item_id: item_id )
+    # 住所の保存
+    Address.create( post_code: post_code, prefecture: prefecture, city: city, address: address, building_name: building_name, tel_num: tel_num ,order_id: order.id )
+  end
+end
