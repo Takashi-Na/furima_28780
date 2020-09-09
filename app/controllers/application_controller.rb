0d484c8994b7f547ff987143695aaf80729a779b
Takashi-Na <Takashi-Na@users.noreply.github.com>
2020-09-09 11:45:43 +0900

    PayJp環境変数

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit( :sign_up, keys: [:nickname, :family_name, :first_name, :kana_family_name, :kana_first_name, :birthday] )
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :kana_family_name, :kana_first_name, :birthday])
   end
 
   def basic_auth
